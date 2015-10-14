c503e57b1ad87b17e81ae49c6e95a8886a1a7a69
Danil Skachkov <danil.skachkov@speaktoit.com>
2015-10-14 14:08:56 +0600

    Change configuration

--- ApiAiSDK/samples/android/Resources/Resource.designer.cs
+++ ApiAiSDK/samples/android/Resources/Resource.designer.cs
@@ -49,7 +49,10 @@ namespace AndroidSample
 			public const int Icon = 2130837504;
 			
 			// aapt resource value: 0x7f020001
-			public const int recording_shape = 2130837505;
+			public const int monoandroidsplash = 2130837505;
+			
+			// aapt resource value: 0x7f020002
+			public const int recording_shape = 2130837506;
 			
 			static Drawable()
 			{
@@ -64,32 +67,32 @@ namespace AndroidSample
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050004
-			public const int buttonCancel = 2131034116;
+			// aapt resource value: 0x7f060004
+			public const int buttonCancel = 2131099652;
 			
-			// aapt resource value: 0x7f050002
-			public const int buttonListen = 2131034114;
+			// aapt resource value: 0x7f060002
+			public const int buttonListen = 2131099650;
 			
-			// aapt resource value: 0x7f050003
-			public const int buttonStopListen = 2131034115;
+			// aapt resource value: 0x7f060003
+			public const int buttonStopListen = 2131099651;
 			
-			// aapt resource value: 0x7f050008
-			public const int contextTextView = 2131034120;
+			// aapt resource value: 0x7f060008
+			public const int contextTextView = 2131099656;
 			
-			// aapt resource value: 0x7f050006
-			public const int progressBar = 2131034118;
+			// aapt resource value: 0x7f060006
+			public const int progressBar = 2131099654;
 			
-			// aapt resource value: 0x7f050005
-			public const int recIndicator = 2131034117;
+			// aapt resource value: 0x7f060005
+			public const int recIndicator = 2131099653;
 			
-			// aapt resource value: 0x7f050007
-			public const int resultTextView = 2131034119;
+			// aapt resource value: 0x7f060007
+			public const int resultTextView = 2131099655;
 			
-			// aapt resource value: 0x7f050001
-			public const int selectLanguageSpinner = 2131034113;
+			// aapt resource value: 0x7f060001
+			public const int selectLanguageSpinner = 2131099649;
 			
-			// aapt resource value: 0x7f050000
-			public const int top_container = 2131034112;
+			// aapt resource value: 0x7f060000
+			public const int top_container = 2131099648;
 			
 			static Id()
 			{
@@ -120,14 +123,14 @@ namespace AndroidSample
 		public partial class String
 		{
 			
+			// aapt resource value: 0x7f040002
+			public const int app_name = 2130968578;
+			
 			// aapt resource value: 0x7f040001
-			public const int app_name = 2130968577;
+			public const int hello = 2130968577;
 			
 			// aapt resource value: 0x7f040000
-			public const int hello = 2130968576;
-			
-			// aapt resource value: 0x7f040002
-			public const int library_name = 2130968578;
+			public const int library_name = 2130968576;
 			
 			static String()
 			{
@@ -135,6 +138,22 @@ namespace AndroidSample
 			}
 			
 			private String()
+			{
+			}
+		}
+		
+		public partial class Style
+		{
+			
+			// aapt resource value: 0x7f050000
+			public const int Mono_Android_Theme_Splash = 2131034112;
+			
+			static Style()
+			{
+				global::Android.Runtime.ResourceIdManager.UpdateIdValues();
+			}
+			
+			private Style()
 			{
 			}
 		}
